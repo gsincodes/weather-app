d132e9d83c203a09458f5a5f7f7b012bd7da1fac
gsincodes <gsincodes@gmail.com>
2025-07-24 00:57:17 +0530

    add 5 day forecast and loading screen

--- src/index.js
+++ src/index.js
@@ -19,24 +19,46 @@ let conditionText = document.querySelector("#condition");
 let descText = document.querySelector("#description");
 let feelsLikeText = document.querySelector("#feels-like");
 let dateText = document.querySelector("#date");
+let humidityText = document.querySelector("#humidity");
+let windspeedText = document.querySelector("#windspeed");
+let cloudcoverText = document.querySelector("#cloudcover");
+let sunriseText = document.querySelector("#sunrise");
+let sunsetText = document.querySelector("#sunset");
+
+let content = document.querySelector("#content");
+
+const loadingData = document.querySelector("#loading-data");
+let forecastBody = document.querySelector("#forecast-body");
 
 let useLocation = 'Jamshedpur';
 
 async function getLocationWeather(location) {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=GEYB8BQQVR87UD3DBMD6LH2WV&contentType=json`);
-    const LocationData = await response.json();
-    console.log(LocationData.currentConditions.conditions);
-    displayWeatherImage(LocationData.currentConditions.conditions);
 
-    const extractedData = dataProcessing(LocationData,LocationData.currentConditions);
-    console.log(extractedData);
-    displayStats(extractedData);
-    console.log(LocationData);
-    console.log(LocationData.resolvedAddress);
+    loadingData.style.display = 'block';
+    todayStats.style.display = 'none';
+    forecast.style.display = 'none';
+    content.classList.add = 'blur';
+
+    try{
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=GEYB8BQQVR87UD3DBMD6LH2WV&contentType=json`);
+        const LocationData = await response.json();
+
+        displayWeatherImage(LocationData.currentConditions.conditions);
+        const extractedData = dataProcessing(LocationData,LocationData.currentConditions);
+        
+        displayStats(extractedData);
+        forecastWeatherDisplay(forecastWeather(LocationData.days));
+    }
+    finally{
+        loadingData.style.display = 'none';
+        todayStats.style.display = 'block';
+        content.classList.remove('blur');
+        forecast.style.display = 'block';
+    }
 }
 
 function dataProcessing(forecast, currentStatus){
-    let myData = [currentStatus.temp, forecast.resolvedAddress, currentStatus.conditions, forecast.description, currentStatus.feelslike, forecast.days[0].datetime];
+    let myData = [forecast.days[0].datetime, currentStatus.temp, forecast.resolvedAddress, currentStatus.conditions, forecast.description, currentStatus.feelslike, currentStatus.humidity, currentStatus.windspeed, currentStatus.cloudcover, currentStatus.sunrise, currentStatus.sunset];
     return myData;
 }
 
@@ -81,12 +103,52 @@ searchButton.addEventListener("click", e => {
 
 function displayStats(dataArray){
 
-    tempText.textContent = `${dataArray[0]}°F`;
-    locationText.textContent = dataArray[1];
-    conditionText.textContent = dataArray[2];
-    descText.textContent = dataArray[3];
-    feelsLikeText.textContent = `Feels like: ${dataArray[4]}°F`;
-    dateText.textContent = `Today - ${dataArray[5]}`;
+    dateText.textContent = `Today, ${dataArray[0]}`;
+    tempText.textContent = `${((dataArray[1]-32)/1.8).toFixed(1)}°C`;
+    locationText.textContent = dataArray[2];
+    conditionText.textContent = dataArray[3];
+    descText.textContent = dataArray[4];
+    feelsLikeText.textContent = `Feels like: ${((dataArray[5]-32)/1.8).toFixed(1)}°C`;
+    humidityText.textContent = `Humidity: ${dataArray[6]}%`;
+    windspeedText.textContent = `Windspeed: ${dataArray[7]} mph`;
+    cloudcoverText.textContent = `Cloudcover: ${dataArray[8]}%`;
+    sunriseText.textContent = `Sunrise Time: ${dataArray[9]}`;
+    sunsetText.textContent = `Sunset Time: ${dataArray[10]}`;
+}
+
+function forecastWeather(forecastArray){
+    let forecastArrayData = [];
+    for(let i in forecastArray){
+        if(i==6){
+            console.log(`this data is from forecastWeather ${forecastArrayData}`);
+            return forecastArrayData;
+        }
+        forecastArrayData.push([forecastArray[i].datetime, forecastArray[i].temp, forecastArray[i].tempmin, forecastArray[i].tempmax, forecastArray[i].humidity]);
+    }
+    return forecastArrayData;
+}
 
 
+
+function forecastWeatherDisplay(myForecastData){
+    forecastBody.textContent = "";
+    for(let i  in myForecastData){
+        if(i==0){
+            continue;
+        }
+        const forecastBodyStat = document.createElement("div");
+        forecastBodyStat.id = `day-${i}`;
+        forecastBodyStat.className = "daily-stat";
+        const forecastDateText = document.createElement("div");
+        forecastDateText.className = 'forecast-date-text';
+        forecastDateText.textContent = `${myForecastData[i][0]}`
+        forecastBodyStat.innerHTML = `
+                                        Temp-${((myForecastData[i][1] - 32)/1.8).toFixed(1)}°C, &ensp;
+                                        Min - ${((myForecastData[i][2] - 32)/1.8).toFixed(1)}°C, &ensp;
+                                        Max - ${((myForecastData[i][3] - 32)/1.8).toFixed(1)}°C, &ensp;
+                                        Humidity - ${myForecastData[i][4]}%.`;
+        forecastBody.appendChild(forecastDateText);
+        forecastBody.appendChild(forecastBodyStat);
+        console.log("forecast data");
+    }
 }
