ff3e8c1ee4aad6498a1b8babb16325d777745e21
gsincodes <gsincodes@gmail.com>
2025-07-26 02:40:18 +0530

    Deployment commit

--- main.js
+++ main.js
@@ -212,7 +212,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "/";
+/******/ 		__webpack_require__.p = "/weather-app/";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
